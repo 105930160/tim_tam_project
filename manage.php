9e16dc4c0f9ed5424f503d37a187defb5a6b1362
sasha <sasha@dogpatch.net>
2025-05-09 16:52:18 +1000

    Added metatext and base empty html structure to manage.php. Next: add html body content.

---
+++ manage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Search Expressions of Interest">
+        <meta name="keywords" content="IT, EOI, Expression of Interest, job applications">
+        <meta name="author" content="Sasha Panisset">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Search EOIs</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
