6f3f7f0378936bd66ac74f1169f89715e1c1c0df
sasha <sasha@dogpatch.net>
2025-05-26 20:24:18 +1000

    updated manage files to use new database details

--- update_eoi.php
+++ update_eoi.php
@@ -2,7 +2,7 @@
 $host = "localhost";         // because XAMPP runs the server locally
 $username = "root";          // default username for XAMPP's MySQL
 $password = "";              // default password is empty in XAMPP
-$database = "dummy_eois";  // replace with the actual name of your database
+$database = "timtam_db";  // replace with the actual name of your database
 
 $dbconn = @mysqli_connect($host,$username,$password,$database);
 
