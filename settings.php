06348b634fcdff8ad975686d08cd2fd90d3d5434
Akira-Ellis <akirafred2@outlook.com>
2025-05-26 20:19:11 +1000

    database upload

--- settings.php
+++ settings.php
@@ -2,7 +2,7 @@
     $host = "localhost";
     $username = "root";
     $password = "";
-    $database = "akira_db";
+    $database = "timtam_db";
 
     $conn = mysqli_connect($host,$username,$password,$database);
     
