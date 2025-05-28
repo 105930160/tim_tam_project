b56801696eda4691123efc53e332966a00f6853f
angle6460 <105930160@student.swin.edu.au>
2025-05-28 22:06:28 +1000

    added comments

--- jobs.php
+++ jobs.php
@@ -54,46 +54,46 @@ if ($conn) {
     $query = "SELECT * FROM job_postings";
     $result = mysqli_query($conn, $query);
     if ($result) {
-        while ($row = mysqli_fetch_assoc($result)) { // getting every job posting in the db
+        while ($row = mysqli_fetch_assoc($result)) { // looping every job posting in the db
             echo "<section class='jobPosting'>";
             echo "<div class='contentHolder'>";
             echo "<div>";
-            echo "<h2>" . $row['title'] . "<br>(REF: " . str_pad($row['id'], 5, '0', STR_PAD_LEFT) . ")</h2>";
+            echo "<h2>" . $row['title'] . "<br>(REF: " . str_pad($row['id'], 5, '0', STR_PAD_LEFT) . ")</h2>"; // format it to hav3 5 digits
             echo "<p>" . $row['description'] . "</p>";
             echo "<p><strong>Salary Range:</strong> $" . $row['salary_lower'] . " - $" . $row['salary_upper'] . " per year</p>";
             echo "<p><strong>Reports To:</strong> " . $row['reports_to'] . "</p>";
             echo "</div>";
             echo "<aside>";
                 echo "<h3>Key Responsibilities</h3>";
-                $query2 = "SELECT responsibilities.description FROM job_postings INNER JOIN responsibilities ON job_postings.id = responsibilities.job_id WHERE job_postings.id = " . $row['id'];
-                $result2 = mysqli_query($conn, $query2);
+                $query_responsibilities = "SELECT responsibilities.description FROM job_postings INNER JOIN responsibilities ON job_postings.id = responsibilities.job_id WHERE job_postings.id = " . $row['id'];
+                $result_responsibilities = mysqli_query($conn, $query_responsibilities);
                 echo "<ol>";
-                    while ($row2 = mysqli_fetch_assoc($result2)) {
-                        echo "<li>" . $row2['description'] . "</li>";
+                    while ($row_responsibilities = mysqli_fetch_assoc($result_responsibilities)) { // getting every responsibility row linked job_id
+                        echo "<li>" . $row_responsibilities['description'] . "</li>";
                     }
                 echo "</ol>";
             echo "</aside>";
             echo "<aside>";
                 echo "<h3>Required Qualifications, Skills, and Attributes</h3>";
-                $query2 = "SELECT skills.description FROM job_postings
+                $query_essential = "SELECT skills.description FROM job_postings
                 INNER JOIN skills ON job_postings.id = skills.job_id
                 WHERE job_postings.id = " . $row['id'] . " AND skills.essential = TRUE"; // this finds skills decription from job id
-                $result2 = mysqli_query($conn, $query2);
+                $result_essential = mysqli_query($conn, $query_essential);
                 echo "<h4>Essential</h4>";
                 echo "<ul>";
-                    while ($row2 = mysqli_fetch_assoc($result2)){
-                        echo "<li>" . $row2['description'] . "</li>";
+                    while ($row_essential = mysqli_fetch_assoc($result_essential)){
+                        echo "<li>" . $row_essential['description'] . "</li>";
                     }
                     
             echo "</ul>";
             echo "<h4>Preferable</h4>";
             echo "<ul>";
-                $query3 = "SELECT skills.description FROM job_postings
+                $query_non_essential = "SELECT skills.description FROM job_postings
                 INNER JOIN skills ON job_postings.id = skills.job_id
                 WHERE job_postings.id = " . $row['id'] . " AND skills.essential = FALSE"; // same again unessential ones
-                $result3 = mysqli_query($conn, $query3);
-                while ($row3 = mysqli_fetch_assoc($result3)){
-                    echo "<li>" . $row3['description'] . "</li>";
+                $result_non_essential = mysqli_query($conn, $query_non_essential);
+                while ($row_non_esstial = mysqli_fetch_assoc($result_non_essential)){
+                    echo "<li>" . $row_non_esstial['description'] . "</li>";
                 }
             echo "</ul>";
         echo "</aside>";
